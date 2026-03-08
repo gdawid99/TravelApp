7fe6b97aec885df2acd18f5b538d2fb3097b50fc
GDawid99 <gabrysdaw@gmail.com>
2026-03-08 19:43:13 +0100

    store refreshToken in httpOnly cookie

--- backend/Controllers/AuthController.cs
+++ backend/Controllers/AuthController.cs
@@ -41,24 +41,55 @@ namespace TravelApp.Controllers
             }
 
             var res = await _tokenService.GenerateTokenAsync(user);
-            return Ok(res);
+
+            Response.Cookies.Append("refreshToken", res.RefreshToken, new CookieOptions
+            {
+                HttpOnly = true,
+                Secure = false, //temporarily, remember to change when the protocol switched to https
+                SameSite = SameSiteMode.Strict,
+                Expires = res.RefreshTokenExpiresAt
+            });
+
+            return Ok(new
+            {
+                accessToken = res.AccessToken,
+                userData = user
+            });
         }
 
         [HttpPost("refresh")]
-        public async Task<IActionResult> RefreshToken(TokenInfoDto dto) 
+        public async Task<IActionResult> RefreshToken() 
         {
 
-            var hashedRefreshToken = _tokenService.HashToken(dto.RefreshToken);
+            if (!Request.Cookies.TryGetValue("refreshToken", out var refreshToken))
+            {
+                return Unauthorized("Refresh token not found.");
+            }
+
+            var hashedRefreshToken = _tokenService.HashToken(refreshToken);
 
             var user = await _context.Users.Where(u => u.RefreshToken == hashedRefreshToken).FirstOrDefaultAsync();
 
             if (user is null || user.RefreshTokenExpiresAt < DateTime.UtcNow)
             {
-                return Unauthorized("Invalid or expired refresh token");
+                return Unauthorized("Invalid or expired refresh token.");
             }
 
             var res = await _tokenService.GenerateTokenAsync(user);
-            return Ok(res);
+
+            Response.Cookies.Append("refreshToken", res.RefreshToken, new CookieOptions
+            {
+                HttpOnly = true,
+                Secure = false, //temporarily, remember to change when the protocol switched to https
+                SameSite = SameSiteMode.Strict,
+                Expires = res.RefreshTokenExpiresAt
+            });
+
+            return Ok(new
+            {
+                accessToken = res.AccessToken,
+                userData = user
+            });
         }
     }
 }
